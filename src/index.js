0bfa85d5f8105be6633a9df10cef82205f9566d9
Tejasvi Raj Pant <tejpant@gmail.com>
2020-05-27 21:59:21 +0545

    Set up BrowserRouter

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App/App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
