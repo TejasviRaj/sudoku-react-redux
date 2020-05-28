2f1ec388edb5ce75d93298d4c9519739f4d77c16
Tejasvi Raj Pant <tejpant@gmail.com>
2020-05-28 23:38:42 +0545

    Updates

---
+++ precache-manifest.49c25e9e3800f0da2cc39ccfb9446c4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8901229e49625741a2c2deeb6c60f4d6",
+    "url": "/sudoku-react-redux/index.html"
+  },
+  {
+    "revision": "7c3fd9eaa8b4a2e0c950",
+    "url": "/sudoku-react-redux/static/css/main.52412080.chunk.css"
+  },
+  {
+    "revision": "552094f0c297e60876d1",
+    "url": "/sudoku-react-redux/static/js/2.2d000cca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sudoku-react-redux/static/js/2.2d000cca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c3fd9eaa8b4a2e0c950",
+    "url": "/sudoku-react-redux/static/js/main.64dc2d96.chunk.js"
+  },
+  {
+    "revision": "089d8362cbf5520cc694",
+    "url": "/sudoku-react-redux/static/js/runtime-main.355b22a3.js"
+  }
+]);
